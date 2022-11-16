06c7c5087952daddaa37d827129e990521b8bc11
IB-315168 <315168@via.dk>
2022-11-16 18:19:00 +0100

    Fix - TeamRepo method naming

--- src/main/java/com/sep3yg9/njorddata/repos/TeamRepository.java
+++ src/main/java/com/sep3yg9/njorddata/repos/TeamRepository.java
@@ -3,11 +3,9 @@ package com.sep3yg9.njorddata.repos;
 import com.sep3yg9.njorddata.models.TeamEntity;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
-
 public interface TeamRepository extends CrudRepository<TeamEntity, Integer>
 {
     TeamEntity findByIdteam(int id);
     TeamEntity findByName(String name);
-    TeamEntity findByTeamLeader(int id);
+    TeamEntity findByTeamleader(int id);
 }
