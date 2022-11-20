f33f3e7de8bb26cc1262b31de23f16a6613cc031
IB-315168 <315168@via.dk>
2022-11-20 10:26:16 +0100

    Further exception handling

--- src/main/java/com/sep3yg9/njorddata/grpc/AuthImpl.java
+++ src/main/java/com/sep3yg9/njorddata/grpc/AuthImpl.java
@@ -5,11 +5,11 @@ import com.sep3yg9.njorddata.grpc.protobuf.auth.LoginRequest;
 import com.sep3yg9.njorddata.grpc.protobuf.user.User;
 import com.sep3yg9.njorddata.models.UserEntity;
 import com.sep3yg9.njorddata.services.UserServiceImpl;
+import io.grpc.Status;
 import io.grpc.stub.StreamObserver;
 import org.lognet.springboot.grpc.GRpcService;
 
-@GRpcService
-public class AuthImpl extends AuthServiceGrpc.AuthServiceImplBase
+@GRpcService public class AuthImpl extends AuthServiceGrpc.AuthServiceImplBase
 {
   private final UserServiceImpl userService;
 
@@ -18,24 +18,40 @@ public class AuthImpl extends AuthServiceGrpc.AuthServiceImplBase
     this.userService = userService;
   }
 
-  @Override
-  public void login(LoginRequest request, StreamObserver<User> responseObserver) {
+  @Override public void login(LoginRequest request,
+      StreamObserver<User> responseObserver)
+  {
     UserEntity record = userService.getByEmail(request.getEmail());
 
-    if(record != null && record.getPassword().equals(request.getPassword())) {
-      User user = User.newBuilder()
-          .setId(record.getIdmember())
-          .setFullName(record.getFullName())
-          .setEmail(record.getEmail())
-          .setUserName(record.getUserName())
-          .setPassword(record.getPassword())
-          .build();
+    try
+    {
+      if (record != null && record.getPassword().equals(request.getPassword()))
+      {
+        User user = User.newBuilder().setId(record.getIdmember())
+            .setFullName(record.getFullName()).setEmail(record.getEmail())
+            .setUserName(record.getUserName()).setPassword(record.getPassword())
+            .build();
 
-      responseObserver.onNext(user);
-      responseObserver.onCompleted();
-    } else {
-      System.out.println("Email or password mismatch.");
-      responseObserver.onCompleted();
+        responseObserver.onNext(user);
+        responseObserver.onCompleted();
+      }
+      else
+      {
+        throw new IllegalArgumentException("Email or password mismatch");
+      }
+    }
+    catch (Exception e)
+    {
+      Status status;
+      if (e instanceof IllegalArgumentException)
+      {
+        status = Status.FAILED_PRECONDITION.withDescription(e.getMessage());
+      }
+      else
+      {
+        status = Status.INTERNAL.withDescription(e.getMessage());
+      }
+      responseObserver.onError(status.asRuntimeException());
     }
   }
 }
--- src/main/java/com/sep3yg9/njorddata/grpc/TeamImpl.java
+++ src/main/java/com/sep3yg9/njorddata/grpc/TeamImpl.java
@@ -2,7 +2,6 @@ package com.sep3yg9.njorddata.grpc;
 
 import com.google.protobuf.Empty;
 import com.google.protobuf.Int32Value;
-import com.google.protobuf.StringValue;
 import com.sep3yg9.njorddata.grpc.protobuf.team.CreatingTeam;
 import com.sep3yg9.njorddata.grpc.protobuf.team.Team;
 import com.sep3yg9.njorddata.grpc.protobuf.team.TeamServiceGrpc;
@@ -13,132 +12,157 @@ import com.sep3yg9.njorddata.models.TeamMember;
 import com.sep3yg9.njorddata.models.UserEntity;
 import com.sep3yg9.njorddata.services.TeamServiceImpl;
 import com.sep3yg9.njorddata.services.UserServiceImpl;
+import io.grpc.Status;
 import io.grpc.stub.StreamObserver;
 import org.lognet.springboot.grpc.GRpcService;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@GRpcService
-public class TeamImpl extends TeamServiceGrpc.TeamServiceImplBase
+@GRpcService public class TeamImpl extends TeamServiceGrpc.TeamServiceImplBase
 {
-    private final TeamServiceImpl teamService;
-    private final UserServiceImpl userService;
+  private final TeamServiceImpl teamService;
+  private final UserServiceImpl userService;
 
-    public TeamImpl(TeamServiceImpl teamService, UserServiceImpl userService)
+  public TeamImpl(TeamServiceImpl teamService, UserServiceImpl userService)
+  {
+    this.teamService = teamService;
+    this.userService = userService;
+  }
+
+  @Override public void createTeam(CreatingTeam team,
+      StreamObserver<Team> responseObserver)
+  {
+    TeamEntity nameCheck = teamService.getByName(team.getName());
+
+    try
     {
-        this.teamService = teamService;
-        this.userService = userService;
-    }
+      if (nameCheck != null)
+      {
+        throw new IllegalArgumentException("Name is in use");
+      }
 
-    @Override public void createTeam(CreatingTeam team, StreamObserver<Team> responseObserver)
+      teamService.addTeam(
+          new TeamEntity(team.getTeamLeaderId(), team.getName()));
+
+      TeamEntity teamCreated = teamService.getByName(team.getName());
+      UserEntity userLeader = userService.getById(teamCreated.getTeamLeader());
+
+      User teamLeader = User.newBuilder().setId(userLeader.getIdmember())
+          .setFullName(userLeader.getFullName()).setEmail(userLeader.getEmail())
+          .setPassword(userLeader.getPassword()).build();
+
+      Team team1 = Team.newBuilder().setId(teamCreated.getIdTeam())
+          .setTeamLeader(teamLeader).setName(teamCreated.getName()).build();
+
+      responseObserver.onNext(team1);
+      responseObserver.onCompleted();
+    }
+    catch (Exception e)
     {
-        TeamEntity nameCheck = teamService.getByName(team.getName());
-
-        if(nameCheck != null) {
-            System.out.println("Name is in use");
-
-            User teamLeader = userService.getById(nameCheck.getTeamLeader()).convertToUser();
-
-            responseObserver.onNext(Team.newBuilder()
-                .setId(nameCheck.getIdTeam())
-                .setTeamLeader(teamLeader)
-                .build());
-            responseObserver.onCompleted();
-        }
-
-
-        teamService.addTeam(new TeamEntity(team.getTeamLeaderId(), team.getName()));
-
-        TeamEntity teamCreated = teamService.getByName(team.getName());
-        UserEntity userLeader = userService.getById(teamCreated.getTeamLeader());
-
-        User teamLeader = User.newBuilder()
-            .setId(userLeader.getIdmember())
-            .setFullName(userLeader.getFullName())
-            .setEmail(userLeader.getEmail())
-            .setPassword(userLeader.getPassword())
-            .build();
-
-        Team team1 = Team.newBuilder()
-                .setId(teamCreated.getIdTeam())
-                .setTeamLeader(teamLeader)
-                .setName(teamCreated.getName())
-                .build();
-
-        responseObserver.onNext(team1);
-        responseObserver.onCompleted();
+      Status status;
+      if (e instanceof IllegalArgumentException)
+      {
+        status = Status.FAILED_PRECONDITION.withDescription(e.getMessage());
+      }
+      else
+      {
+        status = Status.INTERNAL.withDescription(e.getMessage());
+      }
+      responseObserver.onError(status.asRuntimeException());
     }
+  }
 
-    @Override public void getById(Int32Value id, StreamObserver<Team> responseObserver){
-        TeamEntity team = teamService.getById(id.getValue());
-//        ArrayList<User> members = new ArrayList<>();
-//        for(UserEntity user : team.getMembers()) {
-//            members.add(user.convertToUser());
-//        }
+  @Override public void getById(Int32Value id,
+      StreamObserver<Team> responseObserver)
+  {
+    TeamEntity team = teamService.getById(id.getValue());
 
-        List<TeamMember> members = team.getMembers();
+    try
+    {
+      if (team == null)
+      {
+        throw new IllegalArgumentException("Team does not exist");
+      }
 
-        List<User> users = new ArrayList<>();
-        for(TeamMember member : members) {
-            users.add(member.getUserEntity().convertToUser());
-        }
+      List<TeamMember> members = team.getMembers();
 
-        User teamLeader = userService.getById(team.getTeamLeader()).convertToUser();
+      List<User> users = new ArrayList<>();
+      for (TeamMember member : members)
+      {
+        users.add(member.getUserEntity().convertToUser());
+      }
 
-        Team team1 = Team.newBuilder()
-                .setId(team.getIdTeam())
-                .setName(team.getName())
-                .setTeamLeader(teamLeader)
-                .addAllMembers(users)
-                .build();
+      User teamLeader = userService.getById(team.getTeamLeader())
+          .convertToUser();
 
-        responseObserver.onNext(team1);
-        responseObserver.onCompleted();
+      Team team1 = Team.newBuilder().setId(team.getIdTeam())
+          .setName(team.getName()).setTeamLeader(teamLeader)
+          .addAllMembers(users).build();
+
+      responseObserver.onNext(team1);
+      responseObserver.onCompleted();
     }
-
-    @Override
-    public void deleteTeam(Int32Value id, StreamObserver<Empty> responseObserver) {
-        teamService.removeTeam(id.getValue());
-        responseObserver.onNext(Empty.newBuilder().build());
-        responseObserver.onCompleted();
+    catch (Exception e)
+    {
+      Status status;
+      if (e instanceof IllegalArgumentException)
+      {
+        status = Status.FAILED_PRECONDITION.withDescription(e.getMessage());
+      }
+      else
+      {
+        status = Status.INTERNAL.withDescription(e.getMessage());
+      }
+      responseObserver.onError(status.asRuntimeException());
     }
+  }
 
-    @Override
-    public void updateTeam(UpdatingTeam team, StreamObserver<Empty> responseObserver){
-        teamService.updateTeam(team);
-        responseObserver.onNext(Empty.newBuilder().build());
-        responseObserver.onCompleted();
+  @Override public void deleteTeam(Int32Value id,
+      StreamObserver<Empty> responseObserver)
+  {
+    try
+    {
+      teamService.removeTeam(id.getValue());
+      responseObserver.onNext(Empty.newBuilder().build());
+      responseObserver.onCompleted();
     }
-
-    @Override
-    public void getByName(StringValue name, StreamObserver<Team> responseObserver) {
-//        TeamEntity team = teamService.getByName(name.getValue());
+    catch (Exception e)
+    {
+      Status status;
+      if (e instanceof IllegalArgumentException)
+      {
+        status = Status.FAILED_PRECONDITION.withDescription(e.getMessage());
+      }
+      else
+      {
+        status = Status.INTERNAL.withDescription(e.getMessage());
+      }
+      responseObserver.onError(status.asRuntimeException());
     }
+  }
 
-//    @Override
-//    public void getByUserId(Int32Value id, StreamObserver<TeamList> responseObserver) {
-//        List<BasicTeam> teams = new ArrayList<>();
-//
-//        UserEntity user = userService.getById(id.getValue());
-//
-//        for(TeamMember team : user.getTeams()) {
-//            teams.add(team.getTeamEntity().convertToBasicTeam());
-//        }
-//    }
-//    @Override
-//    public void getByUserId(Int32Value id, StreamObserver<TeamList> responseObserver) {
-//        Optional<TeamMember> teams = teamService.getByUserId(id.getValue());
-//
-//        List<Team> teamsUser = new ArrayList<>();
-//        if(teams.isPresent()) {
-//            for(TeamMember teamMember : teams.stream().toList()){
-//                teamsUser.add(teamMember.getTeamEntity().convertToTeam());
-//            }
-//        }
-//
-//        TeamList list = TeamList.newBuilder().addAllTeam(teamsUser).build();
-//        responseObserver.onNext(list);
-//        responseObserver.onCompleted();
-//    }
+  @Override public void updateTeam(UpdatingTeam team,
+      StreamObserver<Empty> responseObserver)
+  {
+    try
+    {
+      teamService.updateTeam(team);
+      responseObserver.onNext(Empty.newBuilder().build());
+      responseObserver.onCompleted();
+    }
+    catch (Exception e)
+    {
+      Status status;
+      if (e instanceof IllegalArgumentException)
+      {
+        status = Status.FAILED_PRECONDITION.withDescription(e.getMessage());
+      }
+      else
+      {
+        status = Status.INTERNAL.withDescription(e.getMessage());
+      }
+      responseObserver.onError(status.asRuntimeException());
+    }
+  }
 }
