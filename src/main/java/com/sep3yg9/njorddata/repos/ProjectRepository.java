3e143b10be843303c147a96023f78adf0dc9d938
IB-315168 <315168@via.dk>
2022-11-20 09:18:58 +0100

    Project Repo

---
+++ src/main/java/com/sep3yg9/njorddata/repos/ProjectRepository.java
@@ -0,0 +1,9 @@
+package com.sep3yg9.njorddata.repos;
+
+import com.sep3yg9.njorddata.models.ProjectEntity;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProjectRepository extends CrudRepository<ProjectEntity, Integer>
+{
+
+}
