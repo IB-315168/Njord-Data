9f3228ad3a6b1daa2af15692e983de254e4128df
Rojus Paukste <rojus.ratkevicius@gmail.com>
2022-11-20 11:01:31 +0100

    ProjectService implementation

---
+++ src/main/java/com/sep3yg9/njorddata/services/ProjectServiceImpl.java
@@ -0,0 +1,52 @@
+package com.sep3yg9.njorddata.services;
+
+import com.google.type.DateTime;
+import com.sep3yg9.njorddata.grpc.protobuf.project.UpdatingProject;
+import com.sep3yg9.njorddata.models.ProjectEntity;
+import com.sep3yg9.njorddata.repos.ProjectRepository;
+import com.sep3yg9.njorddata.services.interfaces.ProjectService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Optional;
+
+public class ProjectServiceImpl implements ProjectService {
+    @Autowired
+    private ProjectRepository projectRepository;
+
+    @Override
+    public void addProject(ProjectEntity projectEntityRecord) {
+        projectRepository.save(projectEntityRecord);
+    }
+
+    @Override
+    public void updateProject(UpdatingProject project) {
+        Optional<ProjectEntity> projectEntity1 = projectRepository.findById(project.getId());
+
+        ProjectEntity projectEntity = projectEntity1.get();
+        if (projectEntity == null) {
+            System.out.println("Project does not exist");
+        } else {
+            if (!project.getName().isEmpty() && !projectEntity.getName().equals(project.getName())) {
+                projectEntity.setName(project.getName());
+            }
+            if (!projectEntity.getDeadline().equals(project.getDeadline())) {
+                projectEntity.setDeadline(LocalDateTime.of(LocalDate.of(project.getDeadline().getYear(), project.getDeadline().getMonth(), project.getDeadline().getDay()),
+                        LocalTime.of(project.getDeadline().getHour(), project.getDeadline().getMinute())));
+            }
+            projectRepository.save(projectEntity);
+        }
+    }
+
+    @Override
+    public void removeProject(int id) {
+        projectRepository.deleteById(id);
+    }
+
+    @Override
+    public ProjectEntity getById(int id) {
+        return projectRepository.findByIdproject(id);
+    }
+}
