05e3e6588d9fde131493915e82bdbc7268c5c4f8
Rojus Paukste <rojus.ratkevicius@gmail.com>
2022-11-21 18:40:42 +0100

    1. ProjectEntity received new method - convertToBasicProject(), it requires looking into 2. ProjectServiceImpl received new method - getByUserId(int id), though it looks ok, it would also require feedback

--- src/main/java/com/sep3yg9/njorddata/services/interfaces/ProjectService.java
+++ src/main/java/com/sep3yg9/njorddata/services/interfaces/ProjectService.java
@@ -3,10 +3,13 @@ package com.sep3yg9.njorddata.services.interfaces;
 import com.sep3yg9.njorddata.grpc.protobuf.project.UpdatingProject;
 import com.sep3yg9.njorddata.models.ProjectEntity;
 
+import java.util.ArrayList;
+
 public interface ProjectService
 {
     void addProject(ProjectEntity projectEntityRecord);
     void updateProject(UpdatingProject project);
     void removeProject(int id);
     ProjectEntity getById(int id);
+    ArrayList<ProjectEntity> getByUserId(int id);
 }
