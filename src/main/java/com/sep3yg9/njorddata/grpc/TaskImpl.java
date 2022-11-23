9c0e7b5244c21caecbd736a6720f916a48f29c3a
Franciszek Nurkiewicz <318212@via.dk>
2022-11-23 22:38:45 +0100

    TaskImpl methods - add and update

--- src/main/java/com/sep3yg9/njorddata/grpc/TaskImpl.java
+++ src/main/java/com/sep3yg9/njorddata/grpc/TaskImpl.java
@@ -1,8 +1,82 @@
-//package com.sep3yg9.njorddata.grpc;
+package com.sep3yg9.njorddata.grpc;
 
 import com.sep3yg9.njorddata.grpc.protobuf.project.ProjectServiceGrpc;
+import com.sep3yg9.njorddata.grpc.protobuf.task.CreatingTask;
+import com.sep3yg9.njorddata.grpc.protobuf.task.Task;
+import com.sep3yg9.njorddata.grpc.protobuf.task.TaskServiceGrpc;
+import com.sep3yg9.njorddata.grpc.protobuf.task.UpdatingTask;
+import com.sep3yg9.njorddata.models.ProjectEntity;
+import com.sep3yg9.njorddata.models.SpecificTimeConverter;
+import com.sep3yg9.njorddata.models.TaskEntity;
+import com.sep3yg9.njorddata.models.UserEntity;
+import com.sep3yg9.njorddata.services.interfaces.ProjectService;
+import com.sep3yg9.njorddata.services.interfaces.TaskService;
+import com.sep3yg9.njorddata.services.interfaces.UserService;
+import io.grpc.Status;
+import io.grpc.stub.StreamObserver;
 import org.lognet.springboot.grpc.GRpcService;
 
-//@GRpcService public class TaskImpl extends ProjectServiceGrpc.ProjectServiceImplBase {
-//
-//}
+import java.time.LocalDateTime;
+
+@GRpcService public class TaskImpl extends TaskServiceGrpc.TaskServiceImplBase {
+
+    private final TaskService taskService;
+    private final UserService userService;
+
+    public TaskImpl(TaskService taskService, UserService userService) {
+        this.taskService = taskService;
+        this.userService = userService;
+    }
+
+    //needs fix
+    @Override public void createTask(CreatingTask task, StreamObserver<Task> responseObserver){
+        try
+        {
+            UserEntity userEntity = userService.getById(task.getMemberassigned());
+
+            TaskEntity taskCreated = taskService.addTask(
+                    new TaskEntity()
+            );
+
+            Task task1 = taskCreated.convertToTask();
+            responseObserver.onNext(task1);
+            responseObserver.onCompleted();
+        }
+        catch (Exception e){
+            Status status;
+            if(e instanceof IllegalArgumentException)
+            {
+                status = Status.FAILED_PRECONDITION.withDescription(e.getMessage());
+            }
+            else
+            {
+                status = Status.INTERNAL.withDescription(e.getMessage());
+            }
+            responseObserver.onError(status.asRuntimeException());
+        }
+    }
+
+    @Override public void updateTask(UpdatingTask task, StreamObserver<Task> responseObserver){
+        try{
+            taskService.updateTask(task);
+
+            Task task1 = taskService.getById(task.getId()).convertToTask();
+            responseObserver.onNext(task1);
+            responseObserver.onCompleted();
+        }
+        catch (Exception e)
+        {
+            Status status;
+            if(e instanceof IllegalArgumentException)
+            {
+                status = Status.FAILED_PRECONDITION.withDescription(e.getMessage());
+            }
+            else
+            {
+                status = Status.INTERNAL.withDescription(e.getMessage());
+            }
+            responseObserver.onError(status.asRuntimeException());
+        }
+    }
+
+}
